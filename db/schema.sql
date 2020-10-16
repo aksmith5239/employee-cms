8a3b6d4335a754b35751970e742b0891805db5a5
aksmith5239 <aklobby@gmail.com>
2020-10-16 08:58:17 -0600

    updated employee functions

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE employee (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
-    role_id INTEGER UNSIGNED NOT NULL,
-    manager_id INTEGER UNSIGNED NOT NULL);
+    role_id INTEGER UNSIGNED,
+    manager_id INTEGER UNSIGNED);
 
 CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
@@ -17,4 +17,11 @@ CREATE TABLE role (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR (30),
     salary DECIMAL,
+    department_id INTEGER UNSIGNED);
+
+CREATE TABLE manager(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
+    title VARCHAR (30),
     department_id INTEGER UNSIGNED);
