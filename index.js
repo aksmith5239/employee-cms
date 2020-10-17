55d257a3c7224b4041c57526ebcd353b92c0ee82
aksmith5239 <aklobby@gmail.com>
2020-10-17 17:18:17 -0600

    employee logic updated

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if(connection) {
 //view all employees
 listEmployees = () => {
   console.log("All Employees");
-  connection.query(`SELECT employee.*, role.title, role.salary, department.name AS department, CONCAT(manager.first_name, manager.last_name)AS manager FROM employee
+  connection.query(`SELECT employee.*, role.title, department.name AS department, CONCAT(manager.first_name, manager.last_name)AS manager FROM employee
   LEFT JOIN 
   role ON employee.role_id = role.id
   RIGHT JOIN 
@@ -93,6 +93,35 @@ listEmployeesManager = () => {
   });
 };
 
+addEmployee = (first_name, last_name, role_id, manager_id) => {
+  console.log("Add an Employee");
+  connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                  VALUES 
+                  (?, ?, ?, ?)` , function(err, res) {
+    if(err) throw err;
+    console.log(res + "has been entered into the database");
+    return(res);
+    connection.end();
+  })
+}
 
+removeEmployee = (id) => {
+  console.log("Add an Employee");
+  connection.query(`DELETE FROM employee WHERE employee.id = ?` , function(err, res) {
+    if(err) throw err;
+    console.log(res);
+    return(res);
+    connection.end();
+  })
+}
+
+updateEmployee = () => {
+  console.log("Update Employee");
+  connection.query(`UPDATE employee SET first_name = ?, last_name = ?, role_id = ?, manager_id = ? WHERE id = ?`, function(err, res) {
+    if(err)throw err;
+    console.log(res);
+    connection.end;
+  })
+}
 
 // listEmployeesManager();
